4246837e4970078d2dd55415bb31e1d4e1b4f5b7
ToluwaniO <ogunsanyatoluwani@gmail.com>
2017-09-26 13:36:57 -0400

    Added fragment classes

---
+++ app/src/main/java/com/tpad/choremanager/PeopleFragment.java
@@ -0,0 +1,8 @@
+package com.tpad.choremanager;
+
+/**
+ * Created by oguns on 9/26/2017.
+ */
+
+public class PeopleFragment {
+}
---
+++ app/src/main/java/com/tpad/choremanager/ShoppingFragment.java
@@ -0,0 +1,10 @@
+package com.tpad.choremanager;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by oguns on 9/26/2017.
+ */
+
+public class ShoppingFragment extends Fragment {
+}
